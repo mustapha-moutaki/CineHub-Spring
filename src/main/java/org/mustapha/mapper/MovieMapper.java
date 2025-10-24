0e3a3e83e3478b8f37b039ddfb0c3a105dbb18a0
mustapha <mustaphaamoutaki@gmail.com>
2025-10-24 13:33:20 +0100

    complete crud of all entities

--- src/main/java/org/mustapha/mapper/MovieMapper.java
+++ src/main/java/org/mustapha/mapper/MovieMapper.java
@@ -1,19 +1,3 @@
-//package org.mustapha.mapper;
-//
-//import org.mustapha.dto.MovieDTO;
-//import org.mustapha.model.Movie;
-//import org.mapstruct.Mapper;
-//import org.mapstruct.factory.Mappers;
-//
-//@Mapper(componentModel = "spring")
-//public interface MovieMapper {
-//
-//    MovieMapper INSTANCE = Mappers.getMapper(MovieMapper.class);
-//
-//    MovieDTO toDTO(Movie movie);
-//
-//    Movie toEntity(MovieDTO movieDTO);
-//}
 
 package org.mustapha.mapper;
 
